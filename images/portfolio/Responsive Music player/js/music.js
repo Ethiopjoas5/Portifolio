028c985ac576104c01f5bb51e7283395e4b0795a
Ethiopjoas5 <104714680+Ethiopjoas5@users.noreply.github.com>
2025-05-22 22:23:17 +0300

    Add files via upload

---
+++ images/portfolio/Responsive Music player/js/music.js
@@ -0,0 +1,35 @@
+
+let allMusic = [
+    {
+        name: "Ahemed Teshome - Eyorica",
+        artist: "Ahemed Teshome",
+        img: "Track-1",
+        src: "Track-1"
+    },
+    {
+        name: "Lij Mic – Lij-Mic",
+        artist: "Ategam Alugn",
+        img: "Track-2",
+        src: "Track-2"
+    },
+    {
+        name: "Rophnan",
+        artist: "DORZELICS",
+        img: "Track-3",
+        src: "Track-3"
+    },
+    {
+        name: "Teddy Afro",
+        artist: "ETHIOPIA",
+        img: "Track-4",
+        src: "Track-4"
+    },
+    {
+        name: "Dawit Tsige",
+        artist: "Etitu",
+        img: "Track-5",
+        src: "Track-5"
+    },
+
+
+];
